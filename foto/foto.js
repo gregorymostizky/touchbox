8deaabd86a898090c0021c2ab24ac2a1cef4ca39
Gregory Mostizky <gregory.mostizky@gmail.com>
2011-11-01 15:10:27 +0200

    vertical align

--- foto/foto.js
+++ foto/foto.js
@@ -17,6 +17,18 @@ $(document).ready(function() {
     if($('.light_box_wrapper').hasClass('light_box_wrapper_inactive')) {
       $('.overlay').removeClass('overlay_inactive').addClass('overlay_active');
       $('.light_box_wrapper').removeClass('light_box_wrapper_inactive').addClass('light_box_wrapper_active');
+
+      // force image to fit inside the box
+      var image_ratio = $('.light_box_wrapper img')[0].naturalWidth / $('.light_box_wrapper img')[0].naturalHeight;
+      var box_ratio = $('.light_box_wrapper')[0].height / $('.light_box_wrapper')[0].width;
+
+      console.log(image_ratio);
+
+      if (image_ratio > box_ratio) {
+        $('.light_box_wrapper img').css('height','100%');
+      } else {
+        $('.light_box_wrapper img').css('width','100%');
+      }
     }
   });
 
@@ -24,6 +36,8 @@ $(document).ready(function() {
     if($('.light_box_wrapper').hasClass('light_box_wrapper_active')){
       $('.overlay').removeClass('overlay_active').addClass('overlay_inactive');
       $('.light_box_wrapper').removeClass('light_box_wrapper_active').addClass('light_box_wrapper_inactive');
+      $('.light_box_menu_top').removeClass('light_box_menu_top_active').addClass('light_box_menu_top_inactive');
+      $('.light_box_menu_bottom').removeClass('light_box_menu_bottom_active').addClass('light_box_menu_bottom_inactive');
     }
   });
 
