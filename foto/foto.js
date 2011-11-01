932ede6af6394f63e80cd04f7f6c1cc10da360be
Gregory Mostizky <gregory.mostizky@gmail.com>
2011-11-01 13:24:24 +0200

    simple effect of opening

--- foto/foto.js
+++ foto/foto.js
@@ -1,14 +1,28 @@
 $(document).ready(function() {
 
-  /* Bar Resize */
-  $('.bar').click(function(){
-    var bar = $('.bar');
+//  /* Bar Resize */
+//  $('.bar').click(function(){
+//    var bar = $('.bar');
+//
+//    if(bar.hasClass('bar_collapsed')) {
+//      bar.removeClass('bar_collapsed').addClass('bar_expanded');
+//    } else {
+//      bar.removeClass('bar_expanded').addClass('bar_collapsed');
+//    }
+//  });
 
-    if(bar.hasClass('bar_collapsed')) {
-      bar.removeClass('bar_collapsed').addClass('bar_expanded');
-    } else {
-      bar.removeClass('bar_expanded').addClass('bar_collapsed');
+  /* LIGHT BOX POPUP */
+
+  $('.image_box').click(function(){
+    if($('.light_box_wrapper').hasClass('light_box_wrapper_inactive')) {
+      $('.overlay').removeClass('overlay_inactive').addClass('overlay_active');
+      $('.light_box_wrapper').removeClass('light_box_wrapper_inactive').addClass('light_box_wrapper_active');
     }
   });
-
+  $('.overlay').click(function(){
+    if($('.light_box_wrapper').hasClass('light_box_wrapper_active')){
+      $('.overlay').removeClass('overlay_active').addClass('overlay_inactive');
+      $('.light_box_wrapper').removeClass('light_box_wrapper_active').addClass('light_box_wrapper_inactive');
+    }
+  });
 });
