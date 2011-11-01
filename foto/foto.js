8d4ccef557ffd203a501b8f2507b8ae33b03dad8
Gregory Mostizky <gregory.mostizky@gmail.com>
2011-11-01 15:28:37 +0200

    4 pictures (yay)

--- foto/foto.js
+++ foto/foto.js
@@ -13,21 +13,28 @@ $(document).ready(function() {
 
   /* LIGHT BOX POPUP */
 
-  $('.image_box').click(function(){
+  $('.image_box img').click(function(e){
     if($('.light_box_wrapper').hasClass('light_box_wrapper_inactive')) {
+
+      // insert the image
+      var image_src = $(e.srcElement).attr('src');
+      $('.light_box_image_holder').html("<img src='" + image_src + "'>");
+
+      // open the light box
       $('.overlay').removeClass('overlay_inactive').addClass('overlay_active');
       $('.light_box_wrapper').removeClass('light_box_wrapper_inactive').addClass('light_box_wrapper_active');
 
       // force image to fit inside the box
-      var image_ratio = $('.light_box_wrapper img')[0].naturalWidth / $('.light_box_wrapper img')[0].naturalHeight;
-      var box_ratio = $('.light_box_wrapper')[0].height / $('.light_box_wrapper')[0].width;
+      var image_ratio = $('.light_box_image_holder img')[0].naturalWidth / $('.light_box_image_holder img')[0].naturalHeight;
+      var box_ratio = $('.light_box_wrapper')[0].clientWidth / $('.light_box_wrapper')[0].clientHeight;
 
-      console.log(image_ratio);
+      console.log('img_ratio:' + image_ratio);
+      console.log('box_ratio:' + box_ratio);
 
-      if (image_ratio > box_ratio) {
-        $('.light_box_wrapper img').css('height','100%');
+      if (image_ratio < box_ratio) {
+        $('.light_box_image_holder img').css('height','100%');
       } else {
-        $('.light_box_wrapper img').css('width','100%');
+        $('.light_box_image_holder img').css('width','100%');
       }
     }
   });
